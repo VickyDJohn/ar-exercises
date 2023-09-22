b427acdc754356a56ee7fd2fead8af356e05eaa3
Vicky John <vicky.d.john@email.com>
2023-09-22 16:08:36 -0700

    add 3 stores, output number of stores

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -3,4 +3,8 @@ require_relative '../setup'
 puts "Exercise 1"
 puts "----------"
 
-# Your code goes below here ...
+store = Store.create(name: "Burnaby", annual_revenue: "300000", mens_apparel: "True", womens_apparel: "True")
+store = Store.create(name: "Richmond", annual_revenue: "126000", mens_apparel: "False", womens_apparel: "True")
+store = Store.create(name: "Gastown", annual_revenue: "190000", mens_apparel: "True", womens_apparel: "False")
+
+puts stores = Store.count
