349fdde0c896a7bb980393868f7f37cc62808146
Vicky John <vicky.d.john@email.com>
2023-09-22 16:44:49 -0700

    modify boolean values

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -3,8 +3,8 @@ require_relative '../setup'
 puts "Exercise 1"
 puts "----------"
 
-store = Store.create(name: "Burnaby", annual_revenue: "300000", mens_apparel: "True", womens_apparel: "True")
-store = Store.create(name: "Richmond", annual_revenue: "126000", mens_apparel: "False", womens_apparel: "True")
-store = Store.create(name: "Gastown", annual_revenue: "190000", mens_apparel: "True", womens_apparel: "False")
+store = Store.create(name: "Burnaby", annual_revenue: "300000", mens_apparel: true, womens_apparel: true)
+store = Store.create(name: "Richmond", annual_revenue: "126000", mens_apparel: false, womens_apparel: true)
+store = Store.create(name: "Gastown", annual_revenue: "190000", mens_apparel: true, womens_apparel: false)
 
 puts stores = Store.count
